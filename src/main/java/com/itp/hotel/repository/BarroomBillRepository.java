2bee0e8a504ec48d1fc1f0d179f6d6b353463aea
Cham-88D <alexeidz81@gmail.com>
2021-04-12 23:55:18 +0530

    Created BarroomBillRepository

---
+++ src/main/java/com/itp/hotel/repository/BarroomBillRepository.java
@@ -0,0 +1,9 @@
+package com.itp.hotel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itp.hotel.model.BarroomBill;
+
+public interface BarroomBillRepository extends JpaRepository<BarroomBill, Long>{
+
+}
