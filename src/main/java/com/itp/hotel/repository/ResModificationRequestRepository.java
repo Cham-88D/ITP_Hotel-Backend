b6eb390143f5de7e2dc2cefeb193ff914b9e4413
Cham-88D <alexeidz81@gmail.com>
2021-04-12 23:46:47 +0530

    Created ResModificationRequestRepository

---
+++ src/main/java/com/itp/hotel/repository/ResModificationRequestRepository.java
@@ -0,0 +1,9 @@
+package com.itp.hotel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itp.hotel.model.ResModificationRequest;
+
+public interface ResModificationRequestRepository extends JpaRepository<ResModificationRequest, Integer> {
+
+}
