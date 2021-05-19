739c2e1b603cacfb22d5eafe54bbba8aebab3cd9
Cham-88D <alexeidz81@gmail.com>
2021-05-19 11:57:48 +0530

    updated

--- src/main/java/com/itp/hotel/repository/PurchasedItemDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itp.hotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.itp.hotel.model.PurchasedItemDetail;
-
-public interface PurchasedItemDetailRepository extends JpaRepository<PurchasedItemDetail, Integer> {
-
-}
--- src/main/java/com/itp/hotel/repository/SalaryDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itp.hotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.itp.hotel.model.SalaryDetail;
-
-public interface SalaryDetailRepository extends JpaRepository<SalaryDetail, Long> {
-
-}
